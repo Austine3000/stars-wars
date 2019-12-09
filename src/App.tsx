9bc72627c539ba6f6dad8836297778419c1e8a92
Austine3000 <diribe3000@gmail.com>
2019-12-09 11:29:18 +0100

    Removed react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './utils/theme';
 import { GlobalStyles } from './utils/global';
@@ -15,18 +14,15 @@ const App: React.FC = () => {
     setChecked(!checked);
   };
   return (
-    <Router>
-      <ErrorBoundary>
-        <ThemeProvider theme={checked ? lightTheme : darkTheme}>
-          <Navigation toggleTheme={toggleTheme} checked={checked} />
-
-          <GlobalStyles />
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <Route exact path="/" component={HomePage} />
-          </React.Suspense>
-        </ThemeProvider>
-      </ErrorBoundary>
-    </Router>
+    <ErrorBoundary>
+      <ThemeProvider theme={checked ? lightTheme : darkTheme}>
+        <Navigation toggleTheme={toggleTheme} checked={checked} />
+        <GlobalStyles />
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <HomePage />
+        </React.Suspense>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 };
 
